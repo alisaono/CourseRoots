6e2f38624a91942900fed45c9c6da208289eff2b
Alisa Ono <alisao@mit.edu>
2018-01-23 09:46:52 -0500

    Config for production

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,24 @@ var firebase = require("firebase");
 
 var router = express.Router();
 
-var SECRETS = require('../config');
-let fbConfig = {
-  apiKey: SECRETS.FIREBASE.API_KEY,
-  authDomain: SECRETS.FIREBASE.PRJ_ID + ".firebaseapp.com",
-  databaseURL: "https://" + SECRETS.FIREBASE.DB_NAME + ".firebaseio.com",
-  storageBucket: SECRETS.FIREBASE.BUCKET + ".appspot.com",
-};
+let fbConfig;
+if (process.env.NODE_ENV) { // Running on production server
+	var SECRETS = process.env; // Configureation is stored on process environment
+	firebaseConfig = {
+	  apiKey: SECRETS.FIREBASE_API_KEY,
+	  authDomain: SECRETS.FIREBASE_PRJ_ID + ".firebaseapp.com",
+	  databaseURL: "https://" + SECRETS.FIREBASE_DB_NAME + ".firebaseio.com",
+	  storageBucket: SECRETS.FIREBASE_BUCKET + ".appspot.com",
+	};
+} else { // Running on local machine
+	var SECRETS = require('../config');
+  fbConfig = {
+    apiKey: SECRETS.FIREBASE.API_KEY,
+    authDomain: SECRETS.FIREBASE.PRJ_ID + ".firebaseapp.com",
+    databaseURL: "https://" + SECRETS.FIREBASE.DB_NAME + ".firebaseio.com",
+    storageBucket: SECRETS.FIREBASE.BUCKET + ".appspot.com",
+  };
+}
 
 firebase.initializeApp(fbConfig);
 let database = firebase.database();
