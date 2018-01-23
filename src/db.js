a25b6a0b2d2eb2aeb6bb2f3dfb34d2a77a54f111
tsun34 <tingxiao@mit.edu>
2018-01-23 10:44:56 -0500

    create db.js

---
+++ src/db.js
@@ -0,0 +1,17 @@
+'use strict';
+const mongoose = require('mongoose');
+// firebase URI
+const mongoURL = 'your mongoURI goes here';
+const options = {useMongoClient: true};
+
+mongoose.connect(mongoURL, options);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+
+// db error handling
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db connection handling
+db.on('connected', () => console.log('database connected'));
+
+module.exports = db;
