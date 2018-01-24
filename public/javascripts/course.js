0ee30425300e8ba96c2a4d660a6a3b7de9eb377b
Alisa Ono <alisao@mit.edu>
2018-01-23 19:29:09 -0500

    Sorting feature in department notes page

--- public/javascripts/course.js
+++ public/javascripts/course.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
-  let notes = []
+  let notesByTime = []
+  let notesByPopularity = []
+  let notesByYear = []
+  let notesByNumber = []
+
   $.getJSON("/api/notes/"+deptID,function(data){
     if (data === null) {
       $("#notes").append("<p class='no-notes'>No notes available at this time. Check again later!</p>")
       return
     }
     for (let noteID of Object.keys(data)) {
-      notes.push(data[noteID])
+      notesByTime.push(data[noteID])
     }
-    for (let i = 0; i < notes.length; i++) {
-      let note = notes[i]
-      addNoteToRow(note,"#notes")
-    }
+    notesByTime.sort(function compare(a,b) {
+      if (a.upload_time < b.upload_time) return 1
+      if (a.upload_time > b.upload_time) return -1
+      return 0
+    })
+    updateNotes(notesByTime)
   })
 
   $("#search-btn").on('click', function(e) {
@@ -24,7 +30,60 @@ $(document).ready(function(){
       $('#input-warning').modal()
     }
   })
+
+  $("#sort").change(function() {
+    let sortOption = $(this).val()
+    switch(sortOption) {
+      case "upload_time":
+        updateNotes(notesByTime)
+        break
+      case "popularity":
+        if (notesByPopularity.length !== notesByTime.length) {
+          notesByPopularity = notesByTime.slice().sort(function compare(a,b) {
+            if (a.popularity < b.popularity) return 1
+            if (a.popularity > b.popularity) return -1
+            return 0
+          })
+        }
+        updateNotes(notesByPopularity)
+        break
+      case "year":
+        if (notesByYear.length !== notesByTime.length) {
+          notesByYear = notesByTime.slice().sort(function compare(a,b) {
+            if (a.year < b.year) return 1
+            if (a.year > b.year) return -1
+            if (a.term !== b.term) {
+              if (a.term === "Spring") return 1
+              if (b.term === "Fall") retun -1
+            }
+            return 0
+          })
+        }
+        updateNotes(notesByYear)
+        break
+      case "number":
+        if (notesByNumber.length !== notesByTime.length) {
+          notesByNumber = notesByTime.slice().sort(function compare(a,b) {
+            if (a.number > b.number) return 1
+            if (a.number < b.number) return -1
+            return 0
+          })
+        }
+        updateNotes(notesByNumber)
+        break
+      default:
+        return
+    }
+  })
 })
+
+function updateNotes(notes) {
+  $("#notes").empty()
+  for (let i = 0; i < notes.length; i++) {
+    let note = notes[i]
+    addNoteToRow(note,"#notes")
+  }
+}
 
 function addNoteToRow(note,rowID) {
   let uploadTime = stringifyTime(note.upload_time)
@@ -45,7 +104,6 @@ function addNoteToRow(note,rowID) {
     "<br/>by " + instructors.slice(2) + "</p>")
   $cardBody.append("<p class='card-text'>Uploaded " + uploadTime +
     "<br/>by " + note.author + "</p>")
-  // $card.append("<img class='card-img-top' src='http://placehold.it/600x400' alt=''>")
   $card.append($cardBody)
   $wrapper.append($card)
   $(rowID).append($wrapper)
